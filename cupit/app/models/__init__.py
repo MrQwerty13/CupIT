c6855ba24dd10fb9e24de062a8678b4f17da4d6e
MrQwerty13 <pozur07@icloud.com>
2026-08-15 14:36:21 +0300

    q

--- cupit/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Models package."""
-
-from .cafe import Cafe, Product, Sale
-
-__all__ = ["Cafe", "Product", "Sale"]
--- cupit/run.py
+++
@@ -1,6 +0,0 @@
-"""Entry point for running the CupIT application."""
-
-from app.main import main
-
-if __name__ == '__main__':
-    main()
